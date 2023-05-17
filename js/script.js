5eabae432a4a186b018c108c06e36db5c00cc706
TomyPlassy <plassytomas05@gmail.com>
2023-05-16 21:23:02 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Ejercicio 1
+
+let nombre = "Tomas";
+let apellido = "Plassy";
+
+console.log("Nombre Completo:",nombre + " " + apellido);
+
+
+//Ejercicio 2
+
+let num1 = 5;
+let num2 = 5;
+let suma = num1+num2;
+
+console.log("",num1 ,"+",num2,"=",suma);
+
+let num3 = 3+suma;
+
+console.log("3","+",suma,"=",num3);
+
